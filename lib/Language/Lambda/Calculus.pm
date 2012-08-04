b0a113f195dc57b8eca7d88dc04fcb8597a528da
Carl Masak <cmasak@gmail.com>
2012-08-04 12:13:37 +0200

    abstractions are left-associative

--- lib/Language/Lambda/Calculus.pm
+++ lib/Language/Lambda/Calculus.pm
@@ -2,13 +2,57 @@ class Language::Lambda::Calculus {
     my grammar Expression {
         regex TOP { ^ <expression> $ }
 
-        regex expression { <variable> | <abstraction> | <application> }
+        regex expression {
+            <application-step>+ % [\s*]
+        }
+
+        regex application-step { <variable> | <abstraction> | '(' <expression> ')' }
+
         regex variable { <:Letter> }
         regex abstraction { '(' 'λ' <variable> '.' <expression> ')' }
-        regex application { '(' <expression> \s* <expression> ')' }
+    }
+
+    my class AST {
+        method TOP($/) { make $<expression>.ast }
+
+        method expression($/) {
+            make $<application-step>[0].ast;
+            for $<application-step>[1..*] -> $step {
+                make {
+                    :application,
+                    :left($step.ast),
+                    :right($/.ast),
+                };
+            }
+        }
+
+        method application-step($/) {
+            make ($<variable> // $<abstraction> // $<expression>).ast;
+        }
+
+        method variable($/) {
+            make {
+                :variable,
+                :name(~$/),
+            };
+        }
+
+        method abstraction($/) {
+            make {
+                :abstraction,
+                :left(~$<variable>),
+                :right($<expression>.ast),
+            };
+        }
     }
 
     method is_expression($text) {
         ?Expression.parse($text);
+    }
+
+    method ast($text) {
+        my $match = Expression.parse($text, :actions(AST.new))
+            or die "Could not parse '$text'";
+        $match.ast;
     }
 }
