55988e5629161a4467e5ed41aba71be2dca5e171
Carl Masak <cmasak@gmail.com>
2017-03-01 13:16:14 +0100

    Add a lambda-in-browser.js file
    This is a companion to an index.html which can <script>-import
    the bundle.js that results from browserify-compiling this file.
    
    The idea is eventually to be able to generate such an index.html
    from the text in
    https://gist.github.com/masak/8e082999e06bfb4d03b8d12899bbcde5
    with interactive prompts in the middle where one can try out
    the λ calculus.
    
    Still some ways to go, though, since we can't evaluate lambda
    expressions yet. :)

---
+++ lambda-in-browser.js
@@ -0,0 +1,59 @@
+import { ast } from './index';
+
+const FADED_BG_COLORS = [
+    "#ffddaa",
+    "#fedfb1",
+    "#fde2bb",
+    "#fce4b2",
+    "#fbe7cc",
+    "#faeac3",
+    "#f9ecdd",
+    "#f8eee4",
+    "#f7f1ee",
+    "#f6f3f1",
+    "#f5f5f5"
+];
+const ESCAPE_KEY = 27;
+
+$(".js-eval-text").on("focus", function() {
+    $(this).select();
+});
+
+$(".js-eval-text").on("keydown", function(event) {
+    if (event.which === ESCAPE_KEY) {
+        $(this).blur();
+    }
+});
+
+function evaluateJsExpression(event) {
+    let $form = $(event.delegateTarget);
+    event.preventDefault();
+
+    let $evalText = $form.find(".js-eval-text");
+    let jsExpression = $evalText.val();
+    let $result;
+    try {
+        let result = ast(jsExpression).constructor.name;
+        $result = $("<pre></pre>").text(result);
+    }
+    catch (ex) {
+        $result = $("<em></em>").append(
+            $("<pre></pre>").text(ex.message).addClass("js-error")
+        );
+    }
+
+    let $evalResult = $form.find(".js-result");
+    $evalResult.empty()
+    $evalResult.append($result);
+    function fade(n) {
+        $result.css("background-color", FADED_BG_COLORS[n]);
+    }
+    for (let i = 0; i < FADED_BG_COLORS.length; i++) {
+        setTimeout(fade.bind(null, i), i * 50);
+    }
+
+    $evalText.select();
+}
+
+$(".js-eval-form").on("click", ".js-run-button", evaluateJsExpression);
+$(".js-eval-form").on("submit", evaluateJsExpression);
